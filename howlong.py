e2ede68d68dad4027556b4bd9fe85ea219838802
kimhyunuk95 <105341749+kimhyunuk95@users.noreply.github.com>
2022-08-16 13:09:37 +0900

    Delete howlong.py

--- howlong.py
+++
@@ -1,6 +0,0 @@
-from haversine import haversine
-
-def distance(origin_lat, origin_lng, destination_lat, destination_lng):
-    origin = (origin_lat, origin_lng)
-    destination = (destination_lat,destination_lng)
-    return haversine(origin, destination, unit = 'm')
