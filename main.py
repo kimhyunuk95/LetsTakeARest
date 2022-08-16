78b6682aec374d00f3218697a17523d9d856b7a3
kimhyunuk95 <105341749+kimhyunuk95@users.noreply.github.com>
2022-08-16 22:40:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import cafe_distance
+import howlong
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
-result = cafe_distance.get()
-st.write(result)
+df = pd.read_csv('./seoul_info2.csv')
+if df.empty:
+    st.write('없음')
+else :
+    st.write('있음')
+    st.write(df.shape)
+    
+loc_button = Button(label="Get Location")
+loc_button.js_on_event("button_click", CustomJS(code="""
+    navigator.geolocation.getCurrentPosition(
+        (loc) => {
+            document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
+        }
+    )
+    """))
+result = streamlit_bokeh_events(
+    loc_button,
+    events="GET_LOCATION",
+    key="get_location",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+destination_lat = df['위도']
+destination_lng = df['경도']
+origin_lat, origin_lng = result.get("GET_LOCATION")['lat'], result.get("GET_LOCATION")['lon']
+lat_list = destination_lat.tolist()
+lng_list = destination_lng.tolist()
+    
+ds_list = []
+for i in range(len(lat_list)):
+    ds_list.append(howlong.distance(origin_lat, origin_lng, lat_list[i], lng_list[i]))
+df['거리'] = pd.DataFrame(ds_list)
+a = df[['상호지점명','거리']].sort_values(by='거리').head(5).to_string(index=False)
+    
+
+st.write(a)
