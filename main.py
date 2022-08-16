cb6801fdb89cf68425ef4741aa3e605c5edc564e
kimhyunuk95 <105341749+kimhyunuk95@users.noreply.github.com>
2022-08-16 11:35:28 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import json
+import requests
+
+
+def main():
+  with tab1:
+    st.markdown('''
+    # tab1''')
+    st.markdown('---')
+    
+   with tab2:
+    st.markdown('''
+    ## tab2''')
+    st.markdown('---')
