3119febba3fc84c2b163e7a7276efc55b35d61df
kimhyunuk95 <105341749+kimhyunuk95@users.noreply.github.com>
2022-08-17 09:22:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,6 @@ for i in range(len(lat_list)):
 df['거리'] = pd.DataFrame(ds_list)
 a = df[['상호지점명','거리']].sort_values(by='거리').head(5).to_string(index=False)
     
-b = df
+b = df['상호지점명', '위도', '경도']
 st.write(a)
 st.write(b)
